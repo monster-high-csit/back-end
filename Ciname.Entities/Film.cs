bd1e35e45e9c8c72b5483ca586a080a667c1d89f
Sergorila <sid0rov.sa@yandex.ru>
2022-04-09 16:57:29 +0400

    [be-001]:Added Film entity

---
+++ Ciname.Entities/Film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Entities
+{
+    public class Film
+    {
+        public int FilmID { get; set; }
+        public int GengreID { get; set; }
+        public int StudioID { get; set; }
+        public int FilmmakerID{ get; set; }
+        public string Name { get; set; }
+        public byte AgeLimit { get; set; }
+        public string Description { get; set; }
+        public string ShortDescription { get; set; }
+        public int Rating { get; set; }
+        public DateTime Duration { get; set; }
+    }
+}
