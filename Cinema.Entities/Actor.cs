f26eeba0a14392ff36101c8fea664bc0d77f9d20
Sergorila <sid0rov.sa@yandex.ru>
2022-04-18 21:51:27 +0400

    [be-006]: add-validation-for-actor-film-filmmaker-filmstudio

--- Cinema.Entities/Actor.cs
+++ Cinema.Entities/Actor.cs
@@ -1,10 +1,52 @@
-﻿namespace Cinema.Entities
+﻿using Cinema.Helpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Entities
 {
     public class Actor
     {
+        private string _name;
+        private string _surname;
+
         public int ActorID { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+
+        [StringLength(30, ErrorMessage = "Length is bigger than 30.")]
+        [Required]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!value.ConsistOnlyFIO())
+                {
+                    throw new ArgumentException("Incorrect name.");
+                }
+                _name = value;
+            }
+        }
+
+        [StringLength(40, ErrorMessage = "Length is bigger than 40.")]
+        [Required]
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (!value.ConsistOnlyFIO())
+                {
+                    throw new ArgumentException("Incorrect surname.");
+                }
+                _surname = value;
+            }
+        }
+
         public int FilmID { get; set; }
     }
 }
--- Cinema.Entities/FilmMaker.cs
+++ Cinema.Entities/FilmMaker.cs
@@ -1,11 +1,50 @@
-﻿namespace Cinema.Entities
+﻿using Cinema.Helpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinema.Entities
 {
     public class FilmMaker
     {
+        private string _name;
+        private string _surname;
+
         public int FilmMakerId { get; set; }
 
-        public string Name { get; set; }
+        [StringLength(30, ErrorMessage = "Length is bigger than 30.")]
+        [Required]
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!value.ConsistOnlyFIO())
+                {
+                    throw new ArgumentException("Incorrect name.");
+                }
+                _name = value;
+            }
+        }
 
-        public string SurName { get; set; }
+        [StringLength(40, ErrorMessage = "Length is bigger than 40.")]
+        [Required]
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (!value.ConsistOnlyFIO())
+                {
+                    throw new ArgumentException("Incorrect surname.");
+                }
+                _surname = value;
+            }
+        }
     }
 }
